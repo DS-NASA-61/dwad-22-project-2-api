dcdcaf9a2afe41c14353cdd5c172ba9727715ca3
DS-NASA-61 <122330460+DS-NASA-61@users.noreply.github.com>
2023-03-15 18:34:07 +0800

    initial commit

---
+++ src/components/PrayerWall.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class PrayerWall extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <button className="btn btn-primary btn-sm">Add Prayer</button>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
